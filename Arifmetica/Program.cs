fd22dbc14168a22a82650ba5cc0f66760a496f30
Анна Гурьянова <annaguryanova73@gmail.com>
2024-09-30 13:04:16 +0500

    tasl2 с дополнениями

--- Arifmetica/Program.cs
+++ Arifmetica/Program.cs
@@ -18,6 +18,18 @@ namespace Arifmetica
             Console.WriteLine("sin(15°) = " + Math.Round(sin, 3));
             Console.WriteLine("cos(15°) = " + Math.Round(cos, 3));
 
+            Console.WriteLine();
+            Console.WriteLine("Напишите, пожалуйста, значение угла в градусах");
+            angleInDegees = double.Parse(Console.ReadLine());
+
+            angleInRadians = angleInDegees * Math.PI / 180;
+            sin = Math.Sin(angleInRadians);
+            cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(" + angleInDegees + "°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(" + angleInDegees + "°) = " + Math.Round(cos, 3));
+
+
             Console.ReadKey();
         }
     }
