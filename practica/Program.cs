6b314bc6ed189617b4691ccf2e445f5654b5217c
Анна Гурьянова <annaguryanova73@gmail.com>
2024-10-21 15:26:06 +0500

    Задание 7, первые два пункта.

---
+++ practica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение длины ребра:");
+
+            double a = double.Parse(Console.ReadLine());
+
+            double v = (a * a * a * Math.Sqrt(2)) / 3;
+
+            double s = 2 * a * a * Math.Sqrt(3);
+
+            Console.WriteLine("Объём октаэдра:" + Math.Round(v, 3));
+            Console.WriteLine("Объём октаэдра:" + Math.Round(s, 3));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ task7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки (x; y) через пробел:");
+
+            var input = Console.ReadLine();
+
+            var index = input.IndexOf(" "); //индекс пробела в строке
+            var x = double.Parse(input.Substring(0, index));
+
+            var y = double.Parse(input.Substring(index + 1));
+
+            if (Koordinat(x, y))
+                Console.WriteLine($"Точка ({x}; {y}) лежит в ограниченной области");
+            else
+                Console.WriteLine($"Точка ({x}; {y}) не лежит в ограниченной области");
+
+            Console.ReadKey();
+        }
+        static bool Koordinat(double x, double y)
+        {
+            return y >= 1 || y <= -3;
+        }
+    }
+}
