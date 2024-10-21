5d1e8c7a1cd74352aadbd2f0c45bf225ec9398e0
Анна Гурьянова <annaguryanova73@gmail.com>
2024-10-21 15:30:16 +0500

    Задание 7, первые два пункта.

--- practica/Program.cs
+++ practica/Program.cs
@@ -10,19 +10,7 @@ namespace practica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите значение длины ребра:");
-
-            double a = double.Parse(Console.ReadLine());
-
-            double v = (a * a * a * Math.Sqrt(2)) / 3;
-
-            double s = 2 * a * a * Math.Sqrt(3);
-
-            Console.WriteLine("Объём октаэдра:" + Math.Round(v, 3));
-            Console.WriteLine("Объём октаэдра:" + Math.Round(s, 3));
-
-            Console.ReadKey();
-
+           
         }
     }
 }
