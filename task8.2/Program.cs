ceffadb246e9f931785431d35196eda424fa7627
Анна Гурьянова <annaguryanova73@gmail.com>
2024-11-11 14:36:21 +0500

    Задание 8 выполнено

---
+++ task8.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x}) = {Function(x)}");
+
+            Console.ReadKey();
+        }
+        static double Function(double x)
+        {
+            double p = Math.PI;
+
+            if (x >= p / 2)
+                return 1 / x;
+            else if (x <= -p / 2)
+                return -1 / x;
+            else
+                return Math.Tan(x * p / 180);
+        }
+    }
+}
